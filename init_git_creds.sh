2056ba035f17552f31e8c4d69b420299a910d811
Niarfe <efrain.olivares@gmail.com>
2026-05-24 04:54:05 +0000

    Initial commit

---
+++ init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=<your github email>
+NAME=<your github user name>
+
+git config --global --list
+
+git config --global user.email efrain.olivares@gmail.com
+git config --global user.name  Niarfe
+
+git config --global --list
